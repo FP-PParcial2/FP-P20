bb62555f77fca7cfc0e6935cd4acdac6bf54a3b8
Eazuara <A01410412@itesm.mx>
2016-10-20 20:05:29 -0500

    Update FPP20.java

--- FP-P20/src/fp/p20/FPP20.java
+++ FP-P20/src/fp/p20/FPP20.java
@@ -15,35 +15,35 @@ public class FPP20 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) {//DEFINIR VARIBLES
  int i,suma=0, n1, n2;
         Scanner sc = new Scanner(System.in);
         System.out.print("Introduce primer número: ");
         n1 = sc.nextInt();
         System.out.print("Introduce segundo número: ");
         n2 = sc.nextInt();
-        for(i=1;i<n1;i++){  // for para sumar todos los divisores propios de n1
+        for(i=1;i<n1;i++){  
              if(n1%i==0){
                 suma=suma+i;
              }
         }
-        // si la suma de los divisores de n1 es igual a n2
-        if(suma==n2){
+      
+        if(suma==n2){//SI SUMA ES IGUAL A N2
            suma=0;
-           for(i=1;i<n2;i++){  // sumo los divisores propios de n2
+           for(i=1;i<n2;i++){ 
                 if(n2%i==0){
                    suma=suma+i;
                }
            }
-        // si la suma de los divisores de n2 es igual a n1
-           if(suma==n1){
+    
+           if(suma==n1){//SI  ES IGUAL A N2
               System.out.println("Son Amigos");
            }else{
                    System.out.println("No son amigos");
            }
         }        
         else{
-               System.out.println("No son amigos");
+               System.out.println("No son amigos");//IMPRIMIR
         }
     }
 }
